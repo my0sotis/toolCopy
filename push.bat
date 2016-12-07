9f8ffb00fd17ed3bd752c04d1c1160b59ace4965
elcarimHub <0.bautroi.0@gmail.com>
2016-12-07 18:54:44 +0700

    commit

---
+++ push.bat
@@ -0,0 +1,15 @@
+
+git pull
+git add .
+git commit -m "commit"
+git push github master
+git push myosotis master
+
+git pull
+git add .
+git commit -m "commit"
+git remote add origin https://github.com/elcarimHub/toolCopy.git
+git push -u origin master
+
+
+pause
